0566a3c3a1d4fb9fc3166746d913be57fb6d0249
Nicole F <nicole.freed@gmail.com>
2019-03-05 20:27:07 -0800

    create Map component with Mapbox centered on lng, lat 0, 23

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import ReactMapboxGl, { Layer } from "react-mapbox-gl";
+
+const Mapbox = ReactMapboxGl({
+  accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
+  minZoom: 2
+});
 
 class Map extends Component {
   render() {
     return(
-      <div></div>
+      <Mapbox
+        // eslint-disable-next-line
+        style="mapbox://styles/mapbox/satellite-streets-v10"
+        containerStyle={{
+          height: "100vh",
+          width: "100vw"
+        }}
+        center={[0, 23]}
+        zoom={[1]}
+      >
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{ "icon-image": "marker-15" }}>
+          </Layer>
+      </Mapbox>
     );
   }
 }
