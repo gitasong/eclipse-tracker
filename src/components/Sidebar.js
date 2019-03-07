e01fd09d145b5bd25d4b00bf7ed3061ecd21c6eb
Nicole F <nicole.freed@gmail.com>
2019-03-06 20:15:30 -0800

    style and animate sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../css/Sidebar.css';
 
 class Sidebar extends Component {
   render() {
