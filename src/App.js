6e3278df0cfd9cb1dd8af8135ce979fd6edbcbf9
Nicole F <nicole.freed@gmail.com>
2019-03-06 20:09:57 -0800

    link to and import FontAwesome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './components/Map';
+import { library } from '@fortawesome/fontawesome-svg-core';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBars, faTimes);
 
 class App extends Component {
   render() {
