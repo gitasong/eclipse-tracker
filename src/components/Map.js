548b89117901713a33fc347eb81847dce3a3dac6
Nicole F <nicole.freed@gmail.com>
2019-03-06 20:06:47 -0800

    add symbol layer with country names

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactMapboxGl, { Layer } from "react-mapbox-gl";
+import ReactMapboxGl, { Layer } from 'react-mapbox-gl';
 
 const Mapbox = ReactMapboxGl({
   accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
@@ -20,11 +20,11 @@ class Map extends Component {
         center={[0, 23]}
         zoom={[1]}
       >
-          <Layer
-            type="symbol"
-            id="marker"
-            layout={{ "icon-image": "marker-15" }}>
-          </Layer>
+        <Layer
+          type="symbol"
+          id="marker"
+          layout={{ "icon-image": "marker-15" }}>
+        </Layer>
       </Mapbox>
     );
   }
