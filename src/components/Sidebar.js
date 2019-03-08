cd0c8bd27d6ec94058339ac70546d7d903137a7d
ngodi <albertngodi@gmail.com>
2019-03-08 09:33:29 +0100

    rendered Calender into sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../css/Sidebar.css';
-
+import Calendar from './Calendar';
 
 class Sidebar extends Component {
   render() {
@@ -16,7 +16,7 @@ class Sidebar extends Component {
             className="close"
             onClick={this.props.closeSidebar}
           />
-      
+      <Calendar />  
       </div>
     );
   }
